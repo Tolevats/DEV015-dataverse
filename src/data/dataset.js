6c8de1b9497049604e65c7d9296cc26d0d3eed74
carolina <carolinartz5991@gmail.com>
2024-06-11 21:27:05 -0500

    feacture: guardando mis cambios en data set

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -61,7 +61,7 @@ export default [
     "name": "Westworld",
     "shortDescription": "Parque temático con robots enloquecidos.",
     "description": "Ambientada en un parque temático futurista poblado por androides, 'Westworld' explora la moralidad, la inteligencia artificial y la naturaleza de la conciencia a través de la interacción entre los huéspedes humanos y los anfitriones robóticos.",
-    "imageUrl": "https://lc.cx/ZAdKFx",
+    "imageUrl": https://lc.cx/ZAdKFx",
     "facts": {
       "yearOfRelease": "2016",
       "yearOfEnd": "2022",
@@ -156,7 +156,7 @@ export default [
     "name": "The 100",
     "shortDescription": "Supervivencia en una Tierra postapocalíptica.",
     "description": "Noventa y siete años después de un apocalipsis nuclear, cien delincuentes juveniles son enviados de regreso a la Tierra desde una estación espacial para determinar si el planeta es habitable, enfrentándose a desafíos de supervivencia y conflictos internos.",
-    "imageUrl": "https://www.formulatv.com/images/series/posters/700/771/dest_1.jpg",
+    "imageUrl": "https://www.formulatv.com/images/series/posters/700/771/dest_3.jpg",
     "facts": {
       "yearOfRelease": "2014",
       "yearOfEnd": "2020",
@@ -167,18 +167,94 @@ export default [
     "extraInfo": {
       "numberOfEpisodes": "100",
       "mainCast": "Eliza Taylor, Bob Morley, Marie Avgeropoulos",
-      "ageRange": "15-40"
+      "ageRange": "15-35"
+    }
+  },
+  {
+    "id": "dark",
+    "name": "Dark",
+    "shortDescription": "Misterios temporales en un pueblo alemán.",
+    "description": "En la pequeña ciudad de Winden, desaparecen dos niños, revelando los secretos de cuatro familias que desentrañan un misterio que abarca varias generaciones. 'Dark' explora el concepto del tiempo y cómo los eventos del pasado, presente y futuro están interconectados.",
+    "imageUrl": "https://i.pinimg.com/564x/5f/4e/61/5f4e61600201742ddbfaaab54af667fd.jpg",
+    "facts": {
+      "yearOfRelease": "2017",
+      "yearOfEnd": "2020",
+      "streamingPlatform": "Netflix",
+      "country": "Alemania",
+      "averageRating": "8.8"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "26",
+      "mainCast": "Louis Hofmann, Oliver Masucci, Karoline Eichhorn",
+      "ageRange": "18-45"
+    }
+  },
+  {
+    "id": "the-orville",
+    "name": "The Orville",
+    "shortDescription": "Aventuras espaciales con un toque de comedia.",
+    "description": "Siguiendo a la tripulación de la nave espacial USS Orville, esta serie mezcla drama, comedia y ciencia ficción mientras explora temas de la naturaleza humana, relaciones y dilemas éticos en un futuro lejano.",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BMzBkZGM1OTQtYmIyNi00ZjM4LWJlNGItNGM0ZWVhODU3YWVlXkEyXkFqcGdeQXVyNzY3NzAzOTA@._V1_.jpg",
+    "facts": {
+      "yearOfRelease": "2017",
+      "yearOfEnd": "2022",
+      "streamingPlatform": "Hulu",
+      "country": "Estados Unidos",
+      "averageRating": "8.0"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "39",
+      "mainCast": "Seth MacFarlane, Adrianne Palicki, Penny Johnson Jerald",
+      "ageRange": "16-50"
+    }
+  },
+  {
+    "id": "lost-in-space",
+    "name": "Lost in Space",
+    "shortDescription": "Familia varada en un planeta alienígena.",
+    "description": "Una familia de colonos espaciales que viajan para establecer una nueva vida en un planeta distante debe enfrentar desafíos inesperados y peligrosos cuando su nave se desvía de curso, dejándolos perdidos en un planeta alienígena.",
+    "imageUrl": "https://pics.filmaffinity.com/Perdidos_en_el_espacio_Serie_de_TV-460443844-large.jpg",
+    "facts": {
+      "yearOfRelease": "2018",
+      "yearOfEnd": "2021",
+      "streamingPlatform": "Netflix",
+      "country": "Estados Unidos",
+      "averageRating": "7.3"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "28",
+      "mainCast": "Molly Parker, Toby Stephens, Maxwell Jenkins",
+      "ageRange": "10-40"
+    }
+  },
+  {
+    "id": "star-trek-discovery",
+    "name": "Star Trek: Discovery",
+    "shortDescription": "Nueva tripulación en la USS Discovery.",
+    "description": "La serie sigue las aventuras de la tripulación de la USS Discovery mientras exploran nuevos mundos y formas de vida, enfrentándose a viejos y nuevos enemigos en una misión para preservar la paz en la Federación Unida de Planetas.",
+    "imageUrl": "https://pics.filmaffinity.com/Star_Trek_Discovery_Serie_de_TV-349570845-large.jpg",
+    "facts": {
+      "yearOfRelease": "2017",
+      "yearOfEnd": "presente",
+      "streamingPlatform": "CBS All Access",
+      "country": "Estados Unidos",
+      "averageRating": "7.2"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "55",
+      "mainCast": "Sonequa Martin-Green, Doug Jones, Anthony Rapp",
+      "ageRange": "16-50"
     }
   },
   {
     "id": "the-umbrella-academy",
     "name": "The Umbrella Academy",
-    "shortDescription": "Familia disfuncional de superhéroes.",
-    "description": "Basada en el cómic de Gerard Way, 'The Umbrella Academy' sigue a una familia disfuncional de hermanos adoptados con habilidades extraordinarias que se reúnen tras la muerte de su padre adoptivo y tratan de desentrañar el misterio de su muerte mientras enfrentan una amenaza apocalíptica.",
-    "imageUrl": "https://m.media-amazon.com/images/M/MV5BZWY4YWNiZmUtZmUxZi00ZjM0LWI2NmItMzEyNDBkNjNlNzUzXkEyXkFqcGdeQXVyNDA4NzAwNjY@._V1_FMjpg_UX1000_.jpg",
+    "shortDescription": "Superhéroes disfuncionales salvan el mundo.",
+    "description": "Un grupo de superhéroes adoptados por un excéntrico millonario se reúne después de su muerte para resolver el misterio de su fallecimiento y enfrentarse a la amenaza del apocalipsis, mientras lidian con sus propios problemas familiares y personales.",
+    "imageUrl": "https://www.formulatv.com/images/series/posters/1600/1677/dest_3.jpg
     "facts": {
       "yearOfRelease": "2019",
-      "yearOfEnd": "2023",
+      "yearOfEnd": "presente",
       "streamingPlatform": "Netflix",
       "country": "Estados Unidos",
       "averageRating": "8.0"
@@ -186,7 +262,218 @@ export default [
     "extraInfo": {
       "numberOfEpisodes": "30",
       "mainCast": "Elliot Page, Tom Hopper, David Castañeda",
-      "ageRange": "15-45"
+      "ageRange": "16-40"
+    }
+  },
+  {
+    "id": "the-man-in-the-high-castle",
+    "name": "The Man in the High Castle",
+    "shortDescription": "Distopía donde el Eje ganó la Segunda Guerra Mundial.",
+    "description": "Basada en la novela de Philip K. Dick, la serie presenta una realidad alternativa donde los Aliados perdieron la Segunda Guerra Mundial y Estados Unidos está dividido entre el Imperio Japonés y la Alemania Nazi, mientras un grupo de rebeldes lucha por la libertad.",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BZWEwNzQ4NzUtMWRmYS00NDdiLTg5NDItODA5M2M4YTM0ZTE2XkEyXkFqcGdeQXVyMTAzNjU2NjM1._V1_FMjpg_UX1000_.jpg",
+    "facts": {
+      "yearOfRelease": "2015",
+      "yearOfEnd": "2019",
+      "streamingPlatform": "Amazon Prime Video",
+      "country": "Estados Unidos",
+      "averageRating": "8.0"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "40",
+      "mainCast": "Alexa Davalos, Rufus Sewell, Joel de la Fuente",
+      "ageRange": "18-50"
+    }
+  },
+  {
+    "id": "expediente-x",
+    "name": "Expediente X",
+    "shortDescription": "Agentes del FBI investigan fenómenos paranormales.",
+    "description": "Fox Mulder y Dana Scully, dos agentes del FBI, investigan casos inexplicables conocidos como 'Expedientes X', mientras desentrañan conspiraciones gubernamentales y enfrentan lo desconocido en un intento de descubrir la verdad.",
+    "imageUrl": "https://es.web.img2.acsta.net/pictures/15/12/02/17/27/123504.jpg",
+    "facts": {
+      "yearOfRelease": "1993",
+      "yearOfEnd": "2018",
+      "streamingPlatform": "Fox",
+      "country": "Estados Unidos",
+      "averageRating": "8.6"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "218",
+      "mainCast": "David Duchovny, Gillian Anderson, Mitch Pileggi",
+      "ageRange": "18-50"
+    }
+  },
+  {
+    "id": "rick-and-morty",
+    "name": "Rick and Morty",
+    "shortDescription": "Aventuras interdimensionales de un científico y su nieto.",
+    "description": "Una serie animada que sigue las aventuras interdimensionales del científico loco Rick Sanchez y su nieto Morty, explorando temas de ciencia ficción con un toque de humor negro y absurdo mientras enfrentan situaciones surrealistas y peligrosas.",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BZjRjOTFkOTktZWUzMi00YzMyLThkMmYtMjEwNmQyNzliYTNmXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg",
+    "facts": {
+      "yearOfRelease": "2013",
+      "yearOfEnd": "presente",
+      "streamingPlatform": "Adult Swim",
+      "country": "Estados Unidos",
+      "averageRating": "9.2"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "61",
+      "mainCast": "Justin Roiland, Chris Parnell, Spencer Grammer",
+      "ageRange": "18-40"
+    }
+  },
+  {
+    "id": "sense8",
+    "name": "Sense8",
+    "shortDescription": "Ocho extraños conectados telepáticamente.",
+    "description": "Ocho individuos de diferentes partes del mundo descubren que están mentalmente y emocionalmente conectados. A medida que intentan entender esta conexión, deben encontrar una manera de sobrevivir a los que los ven como una amenaza para el orden mundial.",
+    "imageUrl": "https://i.blogs.es/bef8c4/sense8-finale/1366_2000.jpg",
+    "facts": {
+      "yearOfRelease": "2015",
+      "yearOfEnd": "2018",
+      "streamingPlatform": "Netflix",
+      "country": "Estados Unidos",
+      "averageRating": "8.3"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "24",
+      "mainCast": "Tuppence Middleton, Brian J. Smith, Doona Bae",
+      "ageRange": "18-50"
+    }
+  },
+  {
+    "id": "the-outer-limits",
+    "name": "The Outer Limits",
+    "shortDescription": "Serie antológica de ciencia ficción.",
+    "description": "Una serie antológica que presenta historias independientes sobre ciencia ficción, terror y misterio, cada episodio explora temas de tecnología avanzada, extraterrestres, y los límites de la percepción humana y la realidad.",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BMDljNTg0ZjAtNjU2OC00ZDE4LThhZDYtOGJhZGVhN2YyNzY5XkEyXkFqcGdeQXVyNjc5NjEzNA@@._V1_FMjpg_UX1000_.jpg",
+    "facts": {
+      "yearOfRelease": "1995",
+      "yearOfEnd": "2002",
+      "streamingPlatform": "Showtime",
+      "country": "Estados Unidos",
+      "averageRating": "7.7"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "152",
+      "mainCast": "Kevin Conway, Eric Schneider, Alex Diakun",
+      "ageRange": "18-50"
+    }
+  },
+  {
+    "id": "firefly",
+    "name": "Firefly",
+    "shortDescription": "Aventuras espaciales de una tripulación renegada.",
+    "description": "Ambientada en el año 2517, 'Firefly' sigue a la tripulación de la nave espacial Serenity, una mezcla de renegados y desajustados que navegan por el espacio exterior, enfrentándose a diversos desafíos mientras intentan mantenerse un paso por delante de las autoridades.",
+    "imageUrl": "https://pics.filmaffinity.com/Firefly_Serie_de_TV-864482280-large.jpg",
+    "facts": {
+      "yearOfRelease": "2002",
+      "yearOfEnd": "2003",
+      "streamingPlatform": "Fox",
+      "country": "Estados Unidos",
+      "averageRating": "9.0"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "14",
+      "mainCast": "Nathan Fillion, Gina Torres, Alan Tudyk",
+      "ageRange": "18-45"
+    }
+  },
+  {
+    "id": "futurama",
+    "name": "Futurama",
+    "shortDescription": "Las desventuras de un repartidor en el siglo XXXI.",
+    "description": "Una serie animada que sigue a Philip J. Fry, un repartidor de pizza congelado criogénicamente y despertado mil años en el futuro, mientras trabaja para una empresa de entregas interestelares junto a sus excéntricos compañeros.",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BYWI3MTJkN2UtMTU0Zi00ZjE2LThjMWEtYmQ5YmFiZmU1N2JhXkEyXkFqcGdeQXVyMTMzOTQyOTk1._V1_FMjpg_UX1000_.jpg",
+    "facts": {
+      "yearOfRelease": "1999",
+      "yearOfEnd": "2013",
+      "streamingPlatform": "Fox",
+      "country": "Estados Unidos",
+      "averageRating": "8.4"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "140",
+      "mainCast": "Billy West, Katey Sagal, John DiMaggio",
+      "ageRange": "16-40"
+    }
+  },
+  {
+    "id": "the-twilight-zone",
+    "name": "The Twilight Zone",
+    "shortDescription": "Serie antológica de ciencia ficción y misterio.",
+    "description": "Una serie antológica que explora temas de ciencia ficción, horror y fantasía. Cada episodio presenta una historia única que examina la condición humana, las implicaciones de la tecnología avanzada y los límites de la percepción de la realidad.",
+    "imageUrl": "https://pics.filmaffinity.com/The_Twilight_Zone_Serie_de_TV-351880714-large.jpg",
+    "facts": {
+      "yearOfRelease": "1959",
+      "yearOfEnd": "1964",
+      "streamingPlatform": "CBS",
+      "country": "Estados Unidos",
+      "averageRating": "9.1"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "156",
+      "mainCast": "Rod Serling, Robert McCord, Jay Overholts",
+      "ageRange": "16-50"
+    }
+  },
+  {
+    "id": "the-expanse",
+    "name": "The Expanse",
+    "shortDescription": "Conflictos interestelares en el futuro.",
+    "description": "En un futuro donde la humanidad ha colonizado el sistema solar, 'The Expanse' sigue a un detective y un capitán de nave mientras descubren una conspiración que amenaza la frágil paz entre la Tierra, Marte y el Cinturón de Asteroides.",
+    "imageUrl": "https://m.media-amazon.com/images/S/pv-target-images/1766a1cc94bcd6c04a9cd449e5c2f2f0bdb4fbf9525832ea2c9c170c48ad433e.jpg",
+    "facts": {
+      "yearOfRelease": "2015",
+      "yearOfEnd": "2022",
+      "streamingPlatform": "Amazon Prime Video",
+      "country": "Estados Unidos",
+      "averageRating": "8.5"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "62",
+      "mainCast": "Steven Strait, Shohreh Aghdashloo, Dominique Tipper",
+      "ageRange": "20-45"
+    }
+  },
+  {
+    "id": "battlestar-galactica",
+    "name": "Battlestar Galactica",
+    "shortDescription": "Supervivencia de la humanidad contra los Cylons.",
+    "description": "La serie sigue a los últimos sobrevivientes de la humanidad que huyen a bordo de la nave Battlestar Galactica tras un devastador ataque de los Cylons. Mientras buscan un nuevo hogar, deben enfrentar conflictos internos y amenazas externas.",
+    "imageUrl": "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p185082_b_v13_as.jpg",
+    "facts": {
+      "yearOfRelease": "2004",
+      "yearOfEnd": "2009",
+      "streamingPlatform": "Syfy",
+      "country": "Estados Unidos",
+      "averageRating": "8.7"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "75",
+      "mainCast": "Edward James Olmos, Mary McDonnell, Katee Sackhoff",
+      "ageRange": "18-50"
+    }
+  },
+  {
+    "id": "fringe",
+    "name": "Fringe",
+    "shortDescription": "Investigación de fenómenos paranormales.",
+    "description": "La agente del FBI Olivia Dunham trabaja junto con el científico loco Walter Bishop y su hijo Peter para investigar una serie de eventos inexplicables que están relacionados con una conspiración mayor que involucra universos paralelos y experimentos extraños.",
+    "imageUrl": "https://i.blogs.es/c1b1ec/fringe_keyart1_1920x1080/1024_2000.jpeg",
+    "facts": {
+      "yearOfRelease": "2008",
+      "yearOfEnd": "2013",
+      "streamingPlatform": "Fox",
+      "country": "Estados Unidos",
+      "averageRating": "8.4"
+    },
+    "extraInfo": {
+      "numberOfEpisodes": "100",
+      "mainCast": "Anna Torv, Joshua Jackson, John Noble",
+      "ageRange": "18-50"
     }
   }
+]
+
 ]
